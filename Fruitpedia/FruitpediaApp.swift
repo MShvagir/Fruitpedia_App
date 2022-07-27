487e14f52ce954d52d17ce01a21ca3f49f914847
Marina Shvagir <alira_m@mail.ru>
2022-07-27 12:51:02 +0300

    Initial Commit

---
+++ Fruitpedia/FruitpediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitpediaApp.swift
+//  Fruitpedia
+//
+//  Created by Марина Швагирь on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitpediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
